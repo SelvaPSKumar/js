4882ba957fbd633edc126b9e2c98586ace17274b
SelvaPSKumar <selvasureshkumar486@gmail.com>
2022-03-03 02:47:35 +0530

    Mar-3-Fetch Task

---
+++ 02-MAR-2022/script.js
@@ -0,0 +1,145 @@
+ // Login Form
+
+ $(document).ready(function () {
+    let urlParams = new URLSearchParams(window.location.search);
+    let myParam = urlParams.get('error');
+
+    if (myParam) {
+        $('.modal-footer p').html(myParam);
+        $('.modal-footer p').fadeIn(1000).fadeOut(2000);
+    }
+    $('#Modal').modal('show');
+});
+
+
+
+
+async function getData() {
+    var data = await fetch("https://salixv3qa.radiusdirect.net/coreapi/clientAdminLogin")
+    var data1 = await data.json()
+    console.log(data1);
+    var arr = [];
+
+    var email = document.querySelector("#email")
+    var emailValue = email.value = ""
+    console.log(emailValue);
+
+    var password = document.querySelector("#password")
+    var passwordValue = password.value = ""
+    arr.push(passwordValue)
+    console.log(arr);
+
+    // var tbody = document.getElementById("tbody").setAttribute("scope","row");
+    // tbody.innerHtml = 
+
+}
+getData()
+
+
+
+document.getElementById("actionBtn").addEventListener("click", getData);
+
+// const tablebody = document.querySelector("#table-body");
+// const url = "https://salixv3qa.radiusdirect.net/coreapi/clientAdminLogin";
+
+// var Name = document.getElementById("name");
+// var Gender = document.getElementById("gender");
+// var Email = document.getElementById("email");
+// var Status = document.getElementById("status");
+
+// var list = new Array;
+
+// const getTable = (users) => {
+//     if (users.length > 0) {
+//         var temp = "";
+//         users.forEach(itemData => {
+//             temp += "<tr data-id=${itemData.id}>";
+//             temp += "<td>" + itemData.name + "</td>";
+//             temp += "<td>" + itemData.gender + "</td>";
+//             temp += "<td>" + itemData.email + "</td>";
+//             temp += "<td>" + itemData.status + "</td>";
+//             temp += "<td><button type='button' class='btn btn-outline-success btn-sm' data-action='edit'  data-id='" + itemData.id + "'>Edit</button><button type='button' class='btn btn-outline-danger btn-sm ms-1' data-action='delete' data-id='" + itemData.id + "'>Delete</button></td>";
+
+//         });
+//         tablebody.innerHTML = temp;
+//     }
+
+// }
+
+
+// function renderTable() {
+//     fetch(url, {
+//         method: "GET",
+//         headers: {
+//             "Content-Type": "application/json",
+//             "Authorization": ""
+//         }
+//     })
+//         .then(res => {
+//             res.json()
+//                 .then(data => {
+//                     list = data.data;
+//                     console.log("response1", list);
+//                     getTable(data.data);
+//                 })
+//         })
+// }
+
+// window.addEventListener("load", (event) => {
+//     event.preventDefault();
+//     renderTable();
+// });
+
+// tablebody.addEventListener("click", function (evt) {
+
+
+//     var elem = evt.target;
+//     var action = elem.dataset.action;
+//     var userId = elem.dataset.id;
+//     if (action === "delete") {
+//         console.log("delete", userId);
+
+//         fetch(`${url}/${userId}`, {
+//             method: "DELETE",
+//             headers: {
+//                 "Content-Type": "application/json",
+//                 "Authorization": ""
+//             }
+//         })
+//             .then((data) => {
+//                 console.log(data);
+//                 renderTable();
+//             })
+//     }
+
+//     var elem = evt.target;
+//     var action = elem.dataset.action;
+//     var userId = elem.dataset.id;
+//     if (action === "edit") {
+//         console.log("edit", userId);
+
+//         let obj = list.find(userslist => userslist.id == userId);
+//         console.log("list Array1", obj);
+
+//         window.location.href = "update.html?id=" + obj.id + "&" + "name=" + obj.name + "&" + "email=" + obj.email + "&" + "status=" + obj.status;
+//         console.log("list Array", list)
+//     }
+// });
+
+// function addRedirect() {
+//     window.location.href = "add.html";
+// }
+
+
+
+// Sidebar
+
+let sidebar = document.querySelector(".sidebar");
+let sidebarBtn = document.querySelector(".sidebarBtn");
+sidebarBtn.onclick = function () {
+    sidebar.classList.toggle("active");
+    if (sidebar.classList.contains("active")) {
+        sidebarBtn.classList.replace("bx-menu", "bx-menu-alt-right");
+    } else
+        sidebarBtn.classList.replace("bx-menu-alt-right", "bx-menu");
+}
