9dc13e402bd7627fa3dd979a38e9f8b82aa9e777
SelvaPSKumar <selvasureshkumar486@gmail.com>
2022-03-08 22:09:09 +0530

    Mar-8

--- 04-MAR-2022/New folder/script.js
+++ 04-MAR-2022/New folder/script.js
@@ -1,195 +1,181 @@
-// Login Form
+// Login Form Modal
 
 $(document).ready(function () {
-    $("#myModal").modal('show');
-    $(".btn").click(function () {
-      $("#myModal").modal('hide');
-    });
+  $("#myModal").modal('show');
+  $(".btn").click(function () {
+    $("#myModal").modal('hide');
   });
-  
-  
-  // Sidebar
-  
-  let sidebar = document.querySelector(".sidebar");
-  let sidebarBtn = document.querySelector(".sidebarBtn");
-  sidebarBtn.onclick = function () {
-    sidebar.classList.toggle("active");
-    if (sidebar.classList.contains("active")) {
-      sidebarBtn.classList.replace("bx-menu", "bx-menu-alt-right");
-    } else
-      sidebarBtn.classList.replace("bx-menu-alt-right", "bx-menu");
-  }
-  
-  // Show Password Func
-  
-  const passwordInput = document.getElementById('password');
-  const togglePasswordButton = document.getElementById('toggle-password');
-  
-  togglePasswordButton.addEventListener('click', togglePassword);
-  
-  function togglePassword() {
-    if (passwordInput.type === 'password') {
-      passwordInput.type = 'text';
-      togglePasswordButton.textContent = 'Hide password';
-      togglePasswordButton.setAttribute('aria-label',
-        'Hide password.');
-    } else {
-      passwordInput.type = 'password';
-      togglePasswordButton.textContent = 'Show password';
-      togglePasswordButton.setAttribute('aria-label',
-        'Show password as plain text. ' +
-        'Warning: this will display your password on the screen.');
+});
+
+// Login Form Modal
+
+// Login Auth
+
+function fetchAuth() {
+  // (A) URL & CREDENTIALS
+  var url = "https://salixv3qa.radiusdirect.net/coreapi/clientAdminLogin",
+    credentials = btoa("email:password");
+
+  // (B) FETCH WITH HTTP AUTH
+  fetch(url, {
+    headers: {
+      "Authorization": `Basic ${credentials}`
     }
+  })
+
+    // (C) SERVER RESPONSE
+    .then((result) => {
+      if (result.status != 200) { throw new Error("Bad Server Response"); }
+      return result.text();
+    })
+    .then((response) => {
+      document.getElementById("demoShow").innerHTML = response;
+    })
+
+    // (D) HANDLE ERRORS (OPTIONAL)
+    .catch((error) => { console.log(error); });
+}
+// Login Auth
+
+// Sidebar
+
+let sidebar = document.querySelector(".sidebar");
+let sidebarBtn = document.querySelector(".sidebarBtn");
+sidebarBtn.onclick = function () {
+  sidebar.classList.toggle("active");
+  if (sidebar.classList.contains("active")) {
+    sidebarBtn.classList.replace("bx-menu", "bx-menu-alt-right");
+  } else
+    sidebarBtn.classList.replace("bx-menu-alt-right", "bx-menu");
+}
+
+// Sidebar
+
+// Table Show GET,Edit & Delete
+
+const tablebody = document.querySelector("#tbody");
+const url = "https://salixv3qa.radiusdirect.net/coreapi/v2/notesTasksCatList?category_type=notes";
+
+var Notes = document.getElementById("notes");
+var dateTym = document.getElementById("datTym");
+var idNum = document.getElementById("idN");
+
+var list = new Array;
+
+const getTable = (datas) => {
+  if (datas.length > 0) {
+    var temp = "";
+    datas.forEach(itemData => {
+      temp += "<tr data-id=${itemData.id}>";
+      temp += "<td>" + itemData.notes + "</td>";
+      temp += "<td>" + itemData.datTym + "</td>";
+      temp += "<td>" + itemData.idN + "</td>";
+      temp += "<td><button type='button' class='btn btn-success btn-sm' data-action='edit'  data-id='" + itemData.id + "'>Edit</button><button type='button' class='btn btn-danger btn-sm ms-1' data-action='delete' data-id='" + itemData.id + "'>Dlt</button></td>";
+
+    });
+    tablebody.innerHTML = temp;
   }
-  
-  
-  async function getData() {
-    var data = await fetch(" https://salixv3qa.radiusdirect.net/coreapi/clientAdminLogin")
-    var data1 = await data.json()
-    console.log(data1);
-    var arr = [];
-  
-    var email = document.querySelector("#email")
-    var emailValue = email.value = ""
-    console.log(emailValue);
-  
-    var password = document.querySelector("#password")
-    var passwordValue = password.value = ""
-    arr.push(passwordValue)
-    console.log(arr);
-  
+
+}
+
+function showTable() {
+  fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": "Basic ZW1haWw6cGFzc3dvcmQ="
+    }
+  })
+    .then(res => {
+      res.json()
+        .then(data => {
+          list = data.data;
+          console.log("response1", list);
+          getTable(data.data);
+        })
+    })
+}
+
+window.addEventListener("load", (event) => {
+  event.preventDefault();
+  showTable();
+});
+
+tablebody.addEventListener("click", function (evnt) {
+
+
+  var con = evnt.target;
+  var func = con.dataset.action;
+  var idnN = con.dataset.id;
+  if (action === "delete") {
+    console.log("delete", IdnN);
+
+    fetch(`${url}/${IdnN}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Basic ZW1haWw6cGFzc3dvcmQ="
+      }
+    })
+      .then((data) => {
+        console.log(data);
+        showTable();
+      })
   }
-  getData()
-  
-  
-  document.getElementById("sub").addEventListener("load", getData);
-  
-  
-  // const tablebody = document.querySelector("#tbody");
-  // const url = "https://salixv3qa.radiusdirect.net/coreapi/v2/notesTasksCatList?category_type=notes";
-  
-  // var Notes = document.getElementById("notes");
-  // var dateTym = document.getElementById("datTym");
-  // var idNum = document.getElementById("idN");
-  
-  // var list = new Array;
-  
-  // const getTable = (datas) => {
-  //     if (datas.length > 0) {
-  //         var temp = "";
-  //         datas.forEach(itemData => {
-  //             temp += "<tr data-id=${itemData.id}>";
-  //             temp += "<td>" + itemData.notes + "</td>";
-  //             temp += "<td>" + itemData.datTym + "</td>";
-  //             temp += "<td>" + itemData.idN + "</td>";
-  //             temp += "<td><button type='button' class='btn btn-success btn-sm' data-action='edit'  data-id='" + itemData.id + "'>Edit</button><button type='button' class='btn btn-danger btn-sm ms-1' data-action='delete' data-id='" + itemData.id + "'>Dlt</button></td>";
-  
-  //         });
-  //         tablebody.innerHTML = temp;
-  //     }
-  
-  // }
-  
-  
-  // function showTable() {
-  //     fetch(url, {
-  //         method: "GET",
-  //         headers: {
-  //             "Content-Type": "application/json",
-  //             "Authorization": ""
-  //         }
-  //     })
-  //         .then(res => {
-  //             res.json()
-  //                 .then(data => {
-  //                     list = data.data;
-  //                     console.log("response1", list);
-  //                     getTable(data.data);
-  //                 })
-  //         })
-  // }
-  
-  // window.addEventListener("load", (event) => {
-  //     event.preventDefault();
-  //     showTable();
-  // });
-  
-  // tablebody.addEventListener("click", function (evnt) {
-  
-  
-  //     var con = evnt.target;
-  //     var func = con.dataset.action;
-  //     var idnN = con.dataset.id;
-  //     if (action === "delete") {
-  //         console.log("delete", IdnN);
-  
-  //         fetch(`${url}/${IdnN}`, {
-  //             method: "DELETE",
-  //             headers: {
-  //                 "Content-Type": "application/json",
-  //                 "Authorization": ""
-  //             }
-  //         })
-  //             .then((data) => {
-  //                 console.log(data);
-  //                 showTable();
-  //             })
-  //     }
-  
-  //     var elem = evt.target;
-  //     var action = elem.dataset.action;
-  //     var userId = elem.dataset.id;
-  //     if (action === "edit") {
-  //         console.log("edit", dataId);
-  
-  //         let objec = list.find(dataslist => dataslist.id == dataId);
-  //         console.log("list Array1", objec);
-  
-  //         window.location.href = "update.html?notes=" + objec.notes + "&" + "datTym=" + objec.datTym + "&" + idN=" + objec.idN";
-  //         console.log("list Array", list)
-  //     }
-  // });
-  
-  // function addRedirect() {
-  //     window.location.href = "add.html";
-  // }
-  
-  
-  // Table Post
-  
-  // window.addEventListener('load', (event) => {
-  
-  //     const addBtn = document.querySelector("#subBtn");
-  
-  //      var Notes = document.getElementById("notes");
-  //      var dateTym = document.getElementById("datTym");
-  //      var idNum = document.getElementById("idN");
-  
-  
-  //  	addBtn.addEventListener("click", (e) => {
-  //       console.log("notes", Notes.value)
-  //       console.log("datTym", dateTym.value)
-  //       console.log("idN", idNum.value)
-  
-  //       e.preventDefault();
-  
-  //        fetch( "https://salixv3qa.radiusdirect.net/coreapi/v2/notesCreate", {
-  //        	method:"POST",
-  //         headers:{
-  //         "Content-Type":"application/json",
-  //     	  "Authorization":""
-  //         },
-  //         body:JSON.stringify({
-  //           notes:Notes.value,
-  //           datTym:dateTym.value,
-  //           idN:idNum.value,
-  //          })
-  //        })
-  //        .then(response => response.json())
-  //        .then((data) => {
-  //         console.log(data);
-  
-  //        })
-  //       });
-  
-  // });
+
+  var elem = evt.target;
+  var action = elem.dataset.action;
+  var dataId = elem.dataset.id;
+  if (action === "edit") {
+    console.log("edit", dataId);
+
+    let objec = list.find(dataslist => dataslist.id == dataId);
+    console.log("list Array1", objec);
+
+    window.location.href = "update.html?notes=" + objec.notes + "&" + "datTym=" + objec.datTym + "&" + "idN=" + objec.idN;
+    console.log("list Array", list)
+  }
+});
+
+// Table Show GET,Edit & Delete
+
+// Table Post
+
+window.addEventListener('load', (event) => {
+
+  const addBtn = document.querySelector("#subBtn");
+
+  var Notes = document.getElementById("notes");
+  var dateTym = document.getElementById("datTym");
+  var idNum = document.getElementById("idN");
+
+
+  addBtn.addEventListener("click", (e) => {
+    console.log("notes", Notes.value)
+    console.log("datTym", dateTym.value)
+    console.log("idN", idNum.value)
+
+    e.preventDefault();
+
+    fetch("https://salixv3qa.radiusdirect.net/coreapi/v2/notesCreate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Basic ZW1haWw6cGFzc3dvcmQ="
+      },
+      body: JSON.stringify({
+        notes: Notes.value,
+        datTym: dateTym.value,
+        idN: idNum.value,
+      })
+    })
+      .then(response => response.json())
+      .then((data) => {
+        console.log(data);
+
+      })
+  });
+
+});
+
+// Table Post
---
+++ 08-MAR-2022/script.js
@@ -0,0 +1,228 @@
+// Login Form Modal
+
+$(document).ready(function () {
+    $("#myModal").modal('show');
+    $(".btn").click(function () {
+      $("#myModal").modal('hide');
+    });
+  });
+  // Login Form Modal Funciton End
+  
+  // Login Auth
+  // const ApiUrl = "https://salixv3qa.radiusdirect.net/coreapi/clientAdminLogin"
+  // async function fetchAuthen() {
+  //   const response = await fetch(ApiUrl, {
+  //     method: 'post',
+  //     headers: new Headers({
+  //       'Authorization': 'Basic ' + btoa(email + ":" + password),
+  //       'Content-Type': 'application/json'
+  //     }),
+  //     body: JSON.stringify({
+  //       "email": "admin@v3qard.com",
+  //       "Password": "radius123"
+  //     })
+  //   });
+  //   const posts = await response.json();
+  // }
+
+// let email = 'admin@v3qard.com';
+// let password = 'radius123';
+// let url1 = `https://salixv3qa.radiusdirect.net/coreapi/clientAdminLogin/${email}/${password}`
+// let authString = `${email}:${password}`
+// let headers = new Headers();
+// headers.set('Authorization', 'Basic ' + btoa(authString))
+// fetch(url1,{method: 'POST', headers: headers})
+//     .then(function (response) {
+//         console.log (response)
+//         return response
+//     });
+  
+  function fetchAuth() {
+    // (A) URL & CREDENTIALS
+    const ApiUrl = "https://salixv3qa.radiusdirect.net/coreapi/clientAdminLogin",
+      credentials = btoa("email:password");
+  
+    // (B) FETCH WITH HTTP AUTH
+    fetch(ApiUrl, {
+      method:"POST",
+      headers: {
+        "Authorization": `Basic ${credentials}`
+      }
+    })
+  
+      // (C) SERVER RESPONSE
+      .then((result) => {
+        if (result.status != 200) { throw new Error("Bad Server Response"); }
+        return result.text();
+      })
+      .then((response) => {
+        document.getElementById("demoShow").innerHTML = response;
+      })
+  
+      // (D) HANDLE ERRORS (OPTIONAL)
+      .catch((error) => { console.log(error); });
+  }
+  // Login Auth Function End
+  
+  // Sidebar
+  
+  let sidebar = document.querySelector(".sidebar");
+  let sidebarBtn = document.querySelector(".sidebarBtn");
+  sidebarBtn.onclick = function () {
+    sidebar.classList.toggle("active");
+    if (sidebar.classList.contains("active")) {
+      sidebarBtn.classList.replace("bx-menu", "bx-menu-alt-right");
+    } else
+      sidebarBtn.classList.replace("bx-menu-alt-right", "bx-menu");
+  }
+  // Sidebar Function End
+  
+  // Table Show GET,Edit & Delete
+  
+  const tableBody = document.querySelector("#tbody");
+  const url = "https://salixv3qa.radiusdirect.net/coreapi/v2/notesTasksCatList?category_type=notes";
+  
+  var Notes = document.getElementById("notes");
+  var dataTym = document.getElementById("datTym");
+  var idNum = document.getElementById("idN");
+  
+  var list = new Array;
+  
+  const getTable = (Datas) => {
+    if (Datas.length > 0) {
+      var template = "";
+      Datas.forEach(itemData => {
+        template += "<tr data-id=${itemData.id}>";
+        template += "<td>" + itemData.notes + "</td>";
+        template += "<td>" + itemData.datTym + "</td>";
+        template += "<td>" + itemData.idN + "</td>";
+        template += "<td><button type='button' class='btn btn-success btn-sm' data-funAct='edit'  data-id='" + itemData.id + "'>Edit</button><button type='button' class='btn btn-danger btn-sm ms-1' data-funAct='delete' data-id='" + itemData.id + "'>Delete</button></td>";
+  
+      });
+      tableBody.innerHTML = template;
+    }
+  
+  }
+  
+  function showTable() {
+    fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Basic aWROOnBhc3N3b3Jk"
+      }
+    })
+      .then(res => {
+        res.json()
+          .then(data => {
+            list = data.data;
+            console.log("response1", list);
+            getTable(data.data);
+          })
+      })
+  }
+  
+  window.addEventListener("load", (event) => {
+    event.preventDefault();
+    showTable();
+  });
+  
+  tableBody.addEventListener("click", function (evnt) {
+  
+  
+    var element = evnt.target;
+    var funAct = element.dataset.funAct;
+    var datId = element.dataset.id;
+    if (funAct === "delete") {
+      console.log("delete", datId);
+  
+      fetch(`${url}/${datId}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Basic aWROOnBhc3N3b3Jk"
+        }
+      })
+        .then((data) => {
+          console.log(data);
+          showTable();
+        })
+    }
+  
+    var element = evnt.target;
+    var funAct = element.dataset.funAct;
+    var datId = element.dataset.id;
+    if (funAct === "edit") {
+      console.log("edit", datId);
+  
+      let obj = list.find(userslist => userslist.id == datId);
+      console.log("list Array1", obj);
+  
+      window.location.href = "update.html?notes=" + obj.notes + "&" + "datTym=" + obj.datTym + "&" + "idN=" + obj.idN;
+      console.log("list Array", list)
+    }
+  });
+  // Table Show GET,Edit & Delete Function End
+  
+  // Table Post
+  
+  window.addEventListener('load', (event) => {
+  
+    const addBtn = document.querySelector("#subBtn");
+  
+    var Notes = document.getElementById("notes");
+    var dateTym = document.getElementById("datTym");
+    var idNum = document.getElementById("idN");
+  
+  
+    addBtn.addEventListener("click", (e) => {
+      console.log("notes", Notes.value)
+      console.log("datTym", dateTym.value)
+      console.log("idN", idNum.value)
+  
+      e.preventDefault();
+  
+      fetch("https://salixv3qa.radiusdirect.net/coreapi/v2/notesCreate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Basic aWROOnBhc3N3b3Jk"
+        },
+        body: JSON.stringify({
+          notes: Notes.value,
+          datTym: dateTym.value,
+          idN: idNum.value,
+        })
+      })
+        .then(response => response.json())
+        .then((data) => {
+          console.log(data);
+  
+        })
+    });
+  
+  });
+  // Table Post Function End
+  
+  // Show Password Function
+  
+  // const passwordInput = document.getElementById('password');
+  // const togglePasswordButton = document.getElementById('toggle-password');
+  
+  // togglePasswordButton.addEventListener('click', togglePassword);
+  
+  // function togglePassword() {
+  //   if (passwordInput.type === 'password') {
+  //     passwordInput.type = 'text';
+  //     togglePasswordButton.textContent = 'Hide password';
+  //     togglePasswordButton.setAttribute('aria-label',
+  //       'Hide password.');
+  //   } else {
+  //     passwordInput.type = 'password';
+  //     togglePasswordButton.textContent = 'Show password';
+  //     togglePasswordButton.setAttribute('aria-label',
+  //       'Show password as plain text. ' +
+  //       'Warning: this will display your password on the screen.');
+  //   }
+  // }
+  // Show Password Function End
